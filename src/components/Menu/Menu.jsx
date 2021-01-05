e081cadf8cd5914667ac38c399ab12773e8097a6
Ester Legaz <ester.legaz@gmail.com>
2021-01-05 18:43:56 +0100

    feat(general): include spinner when waiting for promise to be resolved

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,36 +2,54 @@ import React from "react";
 import "./Menu.scss";
 import MenuItem from "./../MenuItem/MenuItem";
 import { listMenuItems } from "./../../services/MenuService";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
+      isLoading: false,
     };
   }
 
   componentDidMount() {
-    listMenuItems().then((data) => {
-      this.setState({
-        items: data,
-      });
+    this.setState({
+      isLoading: true,
     });
+    listMenuItems()
+      .then((data) => {
+        this.setState({
+          items: data,
+        });
+      })
+      .finally(() => {
+        this.setState({
+          isLoading: false,
+        });
+      });
   }
 
   render() {
     return (
-      <ul>
-        {this.state.items.map((item, index) => {
-          return (
-            <MenuItem
-              key={index}
-              title={item.title}
-              description={item.description}
-            />
-          );
-        })}
-      </ul>
+      <>
+        {this.state.isLoading && (
+          <div className="spinner">
+            <CircularProgress />
+          </div>
+        )}
+        <ul>
+          {this.state.items.map((item, index) => {
+            return (
+              <MenuItem
+                key={index}
+                title={item.title}
+                description={item.description}
+              />
+            );
+          })}
+        </ul>
+      </>
     );
   }
 }
